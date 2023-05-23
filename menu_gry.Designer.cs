f0c99cac7700b6ed07caab7cb60a4c75476fadfc
dawid <dawid005c@gmail.com>
2023-05-23 19:29:34 +0200

    files added

---
+++ menu_gry.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace ShipGame
+{
+    partial class menu_gry
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.playerName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(447, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(295, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nazwa użytkownika";
+            // 
+            // playerName
+            // 
+            this.playerName.Location = new System.Drawing.Point(494, 181);
+            this.playerName.Name = "playerName";
+            this.playerName.Size = new System.Drawing.Size(171, 22);
+            this.playerName.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(513, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 62);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Zapisz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menu_gry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ShipGame.Properties.Resources.ship;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1147, 709);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.playerName);
+            this.Controls.Add(this.label1);
+            this.Name = "menu_gry";
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox playerName;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ menu_gry.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShipGame
+{
+    public partial class menu_gry : Form
+    {
+        public menu_gry()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+/*
+            string UserInputSave = TextBox.Text;
+            label2.text = UserInputSave;*/
+        }
+        public class AddPlayer
+        {
+            public string tableName { get; set; }
+            public string playerName { get; set; }
+            public List<Point> ships { get; set; }
+
+
+        }
+
+
+    }
+}
